ef6131179a07494715d3501a2edf0bb9eeb9651a
Cole'y Boy <cohutso>
2021-07-09 14:24:08 -0500

    Added button interactions
    I added the "pedestal" buttons that have a unity event to activate any function in the scene.

---
+++ School VLEs/Assets/ButtonInteractable.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+
+
+public class ButtonInteractable : MonoBehaviour
+{
+    private Interactable interactable;
+    private Hand interHand;
+    
+    public SteamVR_Action_Boolean buttonPress;
+    public Animator buttonAnim;
+    public UnityEvent pressedEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(interactable.isHovering) {
+            if(interHand == null) {
+                interHand = interactable.hoveringHand;
+                buttonPress.AddOnStateDownListener(ButtonPressed, interHand.handType);
+            }    
+        }
+        else {
+
+            if(interHand != null) {
+                buttonPress.RemoveAllListeners(interHand.handType);
+                interHand = null;
+            }
+        }
+    }
+
+    public void ButtonPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource) {
+        StartCoroutine(activateButton());
+        pressedEvent.Invoke();
+    }
+
+    private IEnumerator activateButton() {
+
+        buttonAnim.SetBool("buttonPressed", true);
+
+        yield return new WaitForSeconds(0.2f);
+
+        buttonAnim.SetBool("buttonPressed", false);
+
+    }
+
+}
