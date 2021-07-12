8eb5355d891102f63054fffac2d49981edae622f
Cole'y Boy <cohutso>
2021-07-12 17:05:01 -0500

    Added Functionality to the Exit button

--- School VLEs/Assets/Shelter/SceneLoader.cs
+++ School VLEs/Assets/Shelter/SceneLoader.cs
@@ -9,4 +9,9 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void ReloadScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }
