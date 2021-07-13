a44f1270fbc68480166fa743447e949beb95d5e2
Cole'y Boy <cohutso>
2021-07-13 10:25:32 -0500

    Finished the Challenge Scene 2 Stairs

---
+++ School VLEs/Assets/OpenDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject player;
+    private HingeJoint hinge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = this.GetComponent<HingeJoint>();
+    }
+
+    public void Open() 
+    {
+        JointMotor jm = hinge.motor;
+        jm.targetVelocity = 50;
+        hinge.motor = jm;
+    }
+}
