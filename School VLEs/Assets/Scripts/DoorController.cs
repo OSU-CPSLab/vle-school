d1e28724fc596470110c17d28821ba5511045316
Cole'y Boy <cohutso>
2021-07-12 20:02:05 -0500

    Made a Door Controller
    Door go Brrrr
    door opens when player gets close

---
+++ School VLEs/Assets/Scripts/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private float dist;
+    public GameObject player;
+    private HingeJoint hinge;
+    public float num;
+
+    void Start() 
+    {
+        hinge = this.GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, player.transform.position);
+        if(dist <= num) 
+        {
+            JointMotor jm = hinge.motor;
+            jm.targetVelocity = 50;
+            hinge.motor = jm;
+        }
+    }
+}
