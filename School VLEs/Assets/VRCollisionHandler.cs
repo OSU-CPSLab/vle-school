512c59a3bae46ad465e8a093106bb1911db06fc9
Cole'y Boy <cohutso>
2021-07-13 11:52:02 -0500

    VR Wall Collision Added

---
+++ School VLEs/Assets/VRCollisionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCollisionHandler : MonoBehaviour
+{
+    public GameObject camRig;
+    public float speed = 300;
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        Vector3 movement = transform.forward * -speed * Time.deltaTime;
+        movement.y = 0;
+        camRig.transform.Translate(movement);
+    }
+}
